a66c6d0b962291e4c9e9e51e7371583a4bd2c134
MBleeker <maurits.bleeker@nubisonline.nl>
2017-04-16 12:47:29 +0200

    refactored code a bit

--- icp.m
+++ icp.m
@@ -1,6 +1,8 @@
-function [R, t] = icp(source_file_name, target_file_name, source_type)
-    figure;
-    iters = 30; % max iters 
+function [R, t] = icp(source_file_name, target_file_name, source_type ,plotting, sampling_type)
+    if plotting
+        figure;
+    end
+    
     
     if strcmp(source_type,'.mat')
         P = loadMatrixFromFile(source_file_name);  
@@ -9,43 +11,44 @@ function [R, t] = icp(source_file_name, target_file_name, source_type)
         P = loadPcdFromFile(source_file_name);  
         Q = loadPcdFromFile(target_file_name);
     else 
-        %error('unsuported file, must be .mat or .pcd');
-        %hoezo werkt dit niet 
+        error('unsuported file, must be .mat or .pcd');
     end
-    
+    iters = 30; % max iters 
     n = size(P, 2);
     m = size(Q, 2);
     R =  eye(3);
     t =  zeros(3,1);
-    current_error = inf(1);
+    current_rms = inf(1);
     
     transformed_P = P;
     for itr = 1:iters
-        % brute force is echt veeeeel te traag vor de point clouds 
-        [~, ~, Q_matches] =  matchBruteForce(transformed_P,Q); % miss moeten we hier maar een sample functie van maken ofzo, met als optie bruteforce
+        if strcmp(sampling_type,'brute_force')
+            [~, ~, Q_matches] =  matchBruteForce(transformed_P,Q);
+        else 
+            error('choose a valid sampling type: brute_force,')
+        end    
         
         P_mean = mean(transformed_P,2);
         Q_mean = mean(Q_matches,2);
         
-        %mindist is unused for now
         R_ = calcR(transformed_P,Q_matches, P_mean,Q_mean); 
         R = R_ * R;
         
         t_ = P_mean - R * Q_mean;
         t = R_ * t + t_;
         
-        error = calc_error(transformed_P, Q_matches); 
+        rms = calc_error(transformed_P, Q_matches); 
         transformed_P = R * P - t;
-        
-        clf;
-        hold on; 
-        showPointCloud(transpose(transformed_P), 'b');
-        showPointCloud(transpose(Q), 'r');
-        pause(0.1);
-        
-        if error < current_error || error == 0 || abs(current_error -error < 0.01)
-            current_error =  error;
-            disp(strcat('current error: ', num2str(current_error,3)))
+        if plotting
+            clf;
+            hold on; 
+            showPointCloud(transpose(transformed_P), 'b');
+            showPointCloud(transpose(Q), 'r');
+            pause(0.1);
+        end
+        if rms < current_rms || rms == 0 || abs(current_rms - rms < 0.01)
+            current_rms =  rms;
+            disp(strcat('current error: ', num2str(current_rms,3)))
         else 
             break
         end 
