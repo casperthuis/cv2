07b4da87c69da2ac50e3ab1f4d23790900baea2a
MBleeker <maurits.bleeker@nubisonline.nl>
2017-04-16 12:48:14 +0200

    added two empty functions for unfiform and random sampling

---
+++ random_sampling.m
@@ -0,0 +1,7 @@
+function [ output_args ] = random_sampling( input_args )
+%RANDOM_SAMPLING Summary of this function goes here
+%   Detailed explanation goes here
+
+
+end
+
---
+++ uniform_sapling.m
@@ -0,0 +1,7 @@
+function [ output_args ] = uniform_sapling( input_args )
+%UNIFORM_SAPLING Summary of this function goes here
+%   Detailed explanation goes here
+
+
+end
+
