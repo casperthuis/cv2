3cace3e77251300b390427fa9fda36f697a39ad9
Casper thuis <casper.thuis@gmail.com>
2017-05-02 22:43:58 +0200

    sift match function added

---
+++ Ass2/sift_matches.m
@@ -0,0 +1,12 @@
+function [matches, f1, f2]=sift_matches(image1, image2)
+im1 = im2single( image1 );
+im2 = im2single( image2 );
+
+% get sift features
+[f1, da] = vl_sift(im1);
+[f2, db] = vl_sift(im2);
+
+% Match despribtors
+[matches, ~] = vl_ubcmatch(da, db);
+
+end
