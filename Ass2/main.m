932da340695fcd3dc38165f81dfb4d124c170c7b
Casper thuis <casper.thuis@gmail.com>
2017-05-02 22:44:38 +0200

    sift match function added

--- Ass2/main.m
+++ Ass2/main.m
@@ -4,23 +4,24 @@ function main(im_path1, im_path2)
 % im_pat2 : path to second images, default 'House/frame00000002.png'
 %%%
 % Read two images, single is needed for vlfeat
-im1 = im2single( imread( 'House/frame00000001.png' ) );
-im2 = im2single( imread( 'House/frame00000002.png' ) );
+im1 =  imread( 'House/frame00000001.png' );
+im2 =  imread( 'House/frame00000002.png' );
 
-% get sift features
-[fa, da] = vl_sift(im1);
-[fb, db] = vl_sift(im2);
+[matches, fa, fb] = sift_matches(im1, im2);
 
-% match sift features
-[matches, scores] = vl_ubcmatch(da, db);
 
 N = 20;
+
+
 index = randsample(1:length(matches), N);
 selection = matches(:,index);
 
 
 p1 = fa(1:2, selection(1,:))';
 p2 = fb(1:2, selection(2,:))';
+
+[T, norm_p1] = normalisation(p1);
+[T, norm_p2] = normalisation(p2);
 
 x1 = p1(:,1);
 y1 = p1(:,2);
@@ -35,7 +36,7 @@ F = reshape(V(:,end), [3,3]);
 Df(3,3) = 0;
 F = Uf*Df*Vf';
 
-[T, norm_p] = normalisation([p1; p2])
+
 
 %[transformation, final_sample, ~] = ransac(fa, fb, matches, 8  , 50);
 %[frames1, frames2, matches] = siftmatches(im1, im2)
