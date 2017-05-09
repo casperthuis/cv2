22ae5910e0822e19bb5eeb1bb73556ec49408c2f
MBleeker <maurits.bleeker@nubisonline.nl>
2017-05-09 15:14:43 +0200

    refactor part 2

--- Ass2/point_view_matrix.m
+++ Ass2/point_view_matrix.m
@@ -1,16 +1,21 @@
-function point_view_matrix(num_frames)
-    
+function PVM = point_view_matrix(num_frames)
+    % load the data set from disc
     images_dataset = read_images();
     % take a sample from 1 to num_frame  
     images_dataset =  images_dataset(1:num_frames);
     
+    % to start get the first two images
     im1 = images_dataset{1};
     im2 = images_dataset{2};
     
+    % find matches
     [matches, f1, f2, ~, ~] = find_matches(im1, im2);
     [~, ~, ~, ~, ~, best_inliers] = ransac(matches, f1, f2, 100, 1);
+    
+    % initialise the PVM with all the matches in img1 and img2
     PVM = [matches(1,best_inliers); matches(2,best_inliers)];
     
+    % for frame 2 till num_frames -1, check of there are similar key points
     for itr = 2:(num_frames - 1)
         im1 = images_dataset{itr};
         im2 = images_dataset{itr + 1};
@@ -34,8 +39,6 @@ function point_view_matrix(num_frames)
                     inliers_image2(index) = 0;
                     continue 
                 end
-                next_row = [next_row, 0];
-                continue
             end
             next_row = [next_row, 0];
         end
