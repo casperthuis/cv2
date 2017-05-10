7bd7829f0fb013b2ffd0b33bfd752f9e518f682d
MBleeker <maurits.bleeker@nubisonline.nl>
2017-05-10 21:39:25 +0200

    added code to create D matrix as well

--- Ass2/point_view_matrix.m
+++ Ass2/point_view_matrix.m
@@ -1,4 +1,5 @@
-function PVM = point_view_matrix(num_frames)
+function [PVM, D] = point_view_matrix(num_frames)
+
     % load the data set from disc
     images_dataset = read_images();
     % take a sample from 1 to num_frame  
@@ -10,17 +11,21 @@ function PVM = point_view_matrix(num_frames)
     
     % find matches
     [matches, f1, f2, ~, ~] = find_matches(im1, im2);
-    [~, ~, ~, ~, ~, best_inliers] = ransac(matches, f1, f2, 100, 1);
+    [~, ~, ~, ~, ~, best_inliers] = ransac(matches, f1, f2, 50, 1);
     
     % initialise the PVM with all the matches in img1 and img2
-    PVM = [matches(1,best_inliers); matches(2,best_inliers)];
+    best_inlier_matches = matches(:,best_inliers);
     
-    % for frame 2 till num_frames -1, check of there are similar key points
+    PVM = [best_inlier_matches(1,:); best_inlier_matches(2,:)];
+    D = [f1(1:2,best_inlier_matches(1,:));
+         f2(1:2,best_inlier_matches(2,:))];
+     
     for itr = 2:(num_frames - 1)
         im1 = images_dataset{itr};
         im2 = images_dataset{itr + 1};
        
         [matches, f1, f2, ~, ~] = find_matches(im1, im2);
+        
         [~, ~, ~, ~, ~, best_inliers] = ransac(matches, f1, f2, 100, 1);
 
         inliers_image1 = matches(1,best_inliers);
@@ -28,6 +33,7 @@ function PVM = point_view_matrix(num_frames)
 
         previous_inliers = PVM(size(PVM,1),:);
         next_row = [];
+       
         
         for idx=1:size(previous_inliers, 2)
             number = previous_inliers(idx);
@@ -44,12 +50,35 @@ function PVM = point_view_matrix(num_frames)
         end
         
         new_key_points = nonzeros(inliers_image2)';
-        left_side  =[zeros(size(PVM,1)-1,length(new_key_points)); nonzeros(inliers_image1)'];
-        PVM = [PVM, left_side; next_row, new_key_points];
+        zero_block = [zeros(size(PVM,1)-1,length(new_key_points))];
+        left_side  =[zero_block; nonzeros(inliers_image1)'];
+        new_row = [next_row, new_key_points];
         
-    end  
+        PVM = [PVM, left_side; new_row];
+        
+        non_zeros = nonzeros(new_row);
+        non_zero_idx = [new_row ~= 0];
+        
+        new_row_x = zeros(1, size(new_row, 2));
+        new_row_y = zeros(1, size(new_row, 2));
+        
+        new_row_x(non_zero_idx) = f2(1,non_zeros);
+        new_row_y(non_zero_idx) = f2(2,non_zeros);      
+        
+        new_d_row = [new_row_x;new_row_y];
+        zero_block = zeros((size(PVM,1) - 2) * 2, size(left_side,2));
+        
+        left_side = [zero_block ; [f1(1, nonzeros(inliers_image1));f1(2, nonzeros(inliers_image1)) ]];
+        D = [D, left_side;new_d_row ];
+        
+        
+    end 
     figure;
     imagesc(PVM == 0);
+
+    % for testing, to see of the dense matirx is the same as the PVM matrix
+
+    figure;
+    imagesc(D(1:2:size(D,1),:) == 0);
+   
 end
-
-
