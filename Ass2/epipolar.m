505460f7f3088a22ee9f392eeb98c5adaefd92dd
MBleeker <maurits.bleeker@nubisonline.nl>
2017-05-13 15:44:00 +0200

    make new function for epiplor lines

--- Ass2/epipolar.m
+++ Ass2/epipolar.m
@@ -1,15 +1,24 @@
-function main(im_path1, im_path2)
+function epipolar(im_path1, im_path2)
 %%%
 % im_pat1 : path to first images, default 'House/frame00000001.png'
 % im_pat2 : path to second images, default 'House/frame00000002.png'
 %%%
 % Read two images
-im1 =  imread( 'House/frame00000001.png' );
-im2 =  imread( 'House/frame00000002.png' );
+
+if nargin == 0
+    im_path1 = 'House/frame00000001.png';
+    im_path2 = 'House/frame00000002.png';
+elseif nargin ==  1
+    error('give at least two input pars, not 1')
+end 
+
+
+
+im1 =  imread( im_path1 );
+im2 =  imread(  im_path2 );
 
 % Find sift matches
 [matches, f1, f2, ~, ~] = find_matches(im1, im2);
-
 
 %ransac
 [F, T1, T2, p1_norm, p2_norm, best_inliers] = ransac(matches, f1, f2, 100, 1);
