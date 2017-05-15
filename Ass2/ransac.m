fda0e96018a32613dc7704945b2e48c3b2a946cb
MBleeker <maurits.bleeker@nubisonline.nl>
2017-05-15 11:21:50 +0200

    added some comments

--- Ass2/ransac.m
+++ Ass2/ransac.m
@@ -1,41 +1,41 @@
 function [best_F, best_T1, best_T2, best_inliers_p1, best_inliers_p2, best_inliers_idx] = ransac(matches, f1, f2, N, threshold)
-best_inliers_num = 0;
+    best_inliers_num = 0;
 
-p1 = f1(1:2, matches(1,:));
-p2 = f2(1:2, matches(2,:));
+    p1 = f1(1:2, matches(1,:));
+    p2 = f2(1:2, matches(2,:));
 
 
-for i=1:N
-    % pick random points
-    perm = randperm(size(matches,2),8);
-    p1_set = p1(:, perm);
-    p2_set = p2(:, perm);
-    
-    
-    
-    % normalise points
-    T1 = get_T_matrix(p1_set);
-    T2 = get_T_matrix(p2_set);
-    norm_p1_set = normalisation(T1, p1_set);
-    norm_p2_set = normalisation(T2, p2_set);
-    norm_p1 = normalisation(T1, p1);
-    norm_p2 = normalisation(T2, p2);
-    
-    % Calculate F
-    F = get_F_matrix(norm_p1_set, norm_p2_set);
-    
-    % Count inliers
-    [num_inliers, inliers_p1, inliers_p2, inliers_idx] = count_inliers(F, norm_p1, norm_p2, threshold);
-    
-    % Determine if better previous best
-    if num_inliers > best_inliers_num
-        best_inliers_num = num_inliers;
-        best_F = F;
-        best_inliers_p1 = inliers_p1';
-        best_inliers_p2 = inliers_p2';
-        best_T1 = T1;
-        best_T2 = T2;
-        best_inliers_idx = inliers_idx;
+    for i=1:N
+        % pick 8 random points
+        perm = randperm(size(matches,2),8);
+        p1_set = p1(:, perm);
+        p2_set = p2(:, perm);
+
+
+
+        % normalise points
+        T1 = get_T_matrix(p1_set);
+        T2 = get_T_matrix(p2_set);
+        norm_p1_set = normalisation(T1, p1_set);
+        norm_p2_set = normalisation(T2, p2_set);
+        norm_p1 = normalisation(T1, p1);
+        norm_p2 = normalisation(T2, p2);
+
+        % Calculate F
+        F = get_F_matrix(norm_p1_set, norm_p2_set);
+
+        % Count inliers
+        [num_inliers, inliers_p1, inliers_p2, inliers_idx] = count_inliers(F, norm_p1, norm_p2, threshold);
+
+        % Determine if better than previous best
+        if num_inliers > best_inliers_num
+            best_inliers_num = num_inliers;
+            best_F = F;
+            best_inliers_p1 = inliers_p1';
+            best_inliers_p2 = inliers_p2';
+            best_T1 = T1;
+            best_T2 = T2;
+            best_inliers_idx = inliers_idx;
+        end
     end
-end
 end
--- Ass2/structure_from_motion.m
+++ Ass2/structure_from_motion.m
@@ -1,21 +1,29 @@
 function [] = structure_from_motion(start_frame, end_frame)
-   [PVM, D] = point_view_matrix(start_frame, end_frame);
-   
-   idx_dense = find( sum([D == 0], 1) == 0);
-   dense_block = D(:,idx_dense);
+    % get PVM and the D matrix (which is the PVM but the the x and y coords)
+    [PVM, D] = point_view_matrix(start_frame, end_frame);
     
+    
+    % make a dense block, only use the columns where the intrest point is
+    % present in every view
+    idx_dense = find( sum([D == 0], 1) == 0);
+    dense_block = D(:,idx_dense);
+    
+    % normalise dense block, for every row
     m = mean(dense_block,2);
     dense_block = dense_block - repmat(m, 1, size(dense_block,2));
-   
+
     [U, W, V] = svd(dense_block);
     dense_block = U(:, 1:3) * W(1:3, 1:3) * V(:, 1:3)';
-    
-    
+
+
     M = U(:, 1:3) * W(1:3, 1:3)^(0.5);
     S = W(1:3, 1:3)^(0.5)* V(:, 1:3)';
-    
+
     idx =  ones(1, size(S, 2));
+    
+    % filter out ourliers in the z axis
     treshold = 0.5;
+
     if (~all(S(3,:) < treshold)) 
         idx = idx(S(3,:) < treshold);
         S = S(:, S(3,:) < treshold);
@@ -25,9 +33,12 @@ function [] = structure_from_motion(start_frame, end_frame)
         idx = idx(S(3,:) > -treshold);
         S = S(:, S(3,:) > -treshold);
     end
+    
+    % plot 3D reconstruction
     figure;
     scatter3(S(1,:),S(2,:),S(3,:), 400, idx, '.');
     colormap(summer);
     colorbar;
+    
 end
 
